Backprop through log fixed. Good night :)

main2.py
@@ -1,5 +1,5 @@
 """
-Main script.
+Main script v2 for VAE with Kipft RGCN. We concat the F and E matrix and make A a spare matrix.
 For now it executes the training and evaluation of the RGVAE with radom data.
 Change the parameters in-script. Parsing is yet to come.
 """
@@ -17,9 +17,8 @@ torch.set_default_dtype(my_dtype)
 
 # Parameters
 n = 5
-e = 10      # All random graphs shall have 5 nodes and 10 edges
 d_e = 3
-d_n = 3
+d_n = 2
 
 seed = 11
 np.random.seed(seed=seed)
@@ -27,34 +26,31 @@ torch.manual_seed(seed)
 epochs = 111
 batch_size = 64
 
-train_set = mk_graph_ds(n, d_e, d_n, e, batches=400, batch_size=batch_size)
-test_set = mk_graph_ds(n, d_e, d_n, e, batches=100, batch_size=batch_size)
+train_set = mk_random_graph_ds(n, d_e, d_n, 4000, batch_size=batch_size)
+test_set = mk_random_graph_ds(n, d_e, d_n, 1000, batch_size=batch_size)
 
 model = TorchGVAE(n, d_e, d_n)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-7, weight_decay=5e-4)
 
 for epoch in range(epochs):
     start_time = time.time()
-    with torch.autograd.detect_anomaly():
-        for target in train_set:
-            model.train()
-            mean, logstd = model.encode(target)
-            z = model.reparameterize(mean, logstd)
-            prediction = model.decode(z)
 
-            log_pz = log_normal_pdf(z, torch.zeros_like(z), torch.zeros_like(z))
-            log_qz_x = log_normal_pdf(z, mean, 2*logstd)
-            log_px = mpgm_loss(target, prediction)
-            loss = - torch.mean(log_px + log_pz + log_qz_x)
-            print(loss)
-            print('Epoch {} \n target \n'.format(epoch), target[0])
-            print('prediction \n', prediction[0])
-            loss.backward()
-            optimizer.step()
-            end_time = time.time()
+    for target in train_set:
+        model.train()
+        mean, logstd = model.encode(target)
+        z = model.reparameterize(mean, logstd)
+        prediction = model.decode(z)
+
+        log_pz = log_normal_pdf(z, torch.zeros_like(z), torch.zeros_like(z))
+        log_qz_x = log_normal_pdf(z, mean, 2*logstd)
+        log_px = mpgm_loss(target, prediction)
+        loss = - torch.mean(log_px + log_pz + log_qz_x)
+        print(loss)
+        loss.backward()
+        optimizer.step()
+        end_time = time.time()
 
     # Evaluate
-    print("Start evaluation.")
     mean_loss = []
     with torch.no_grad():
         model.eval()
@@ -68,3 +64,4 @@ for epoch in range(epochs):
             loss = - torch.mean(log_px + log_pz + log_qz_x)
             mean_loss.append(loss)
         print('Epoch: {}, Test set ELBO: {}, time elapse for current epoch: {}'.format(epoch, np.mean(mean_loss), end_time - start_time))
+

models/torch_losses.py
@@ -10,7 +10,7 @@ from models.torch_max_pooling import MPGM
 from models.utils import *
 
 
-def graph_loss(A, E, F, A_hat, E_hat, F_hat):
+def graph_loss(target, prediction, l_A=1., l_E=1., l_F=1.):
     """
     Loss function for the predicted graph. It takes each matrix separately into account.
     Goal is to solve the permutation invariance.
@@ -29,9 +29,8 @@ def graph_loss(A, E, F, A_hat, E_hat, F_hat):
     w4 = 1
 
     # Cast target vectors to tensors.
-    A = torch.tensor(A)
-    E = torch.tensor(E)
-    F = torch.tensor(F)
+    A, E, F = target
+    A_hat, E_hat, F_hat = prediction
 
     # Match number of nodes
     loss_n_nodes = torch.sqrt((torch.count_nonzero(A) - torch.count_nonzero(A_hat))**2)
@@ -48,7 +47,6 @@ def mpgm_loss(target, prediction, l_A=1., l_E=1., l_F=1.):
     A, E, F = target
 
     A_hat, E_hat, F_hat = prediction
-    A_hat, E_hat, F_hat = A_hat.detach(), E_hat.detach(), F_hat.detach()
     bs = A.shape[0]
     n = A.shape[1]
     k = A_hat.shape[1]
@@ -60,7 +58,7 @@ def mpgm_loss(target, prediction, l_A=1., l_E=1., l_F=1.):
     F = torch.tensor(F * 1.)
 
     mpgm = MPGM()
-    X = mpgm.call(A, A_hat, E, E_hat, F, F_hat)
+    X = mpgm.call(A, A_hat.detach(), E, E_hat.detach(), F, F_hat.detach())
 
     # now comes the loss part from the paper:s
     A_t = torch.transpose(X, 2, 1) @ A @ X     # shape (bs,k,n)
@@ -89,9 +87,10 @@ def mpgm_loss(target, prediction, l_A=1., l_E=1., l_F=1.):
     log_p_A = term_1 + term_2 + term_3
 
     # Man so many confusions: is the log over one or both Fs???
-    log_p_F = (1/n) * torch.sum(replace_inf(torch.log(torch.sum((F * F_hat_t), -1))), -1).unsqueeze(-1)
+    log_p_F = (1/n) * torch.sum(torch.log(add_e7(torch.sum((F * F_hat_t), -1))), -1).unsqueeze(-1)
 
-    log_p_E = ((1/(torch.norm(A, p='fro', dim=[-2,-1])-n)) * torch.sum(replace_inf(torch.log(torch.sum(E * E_hat_t, -1))), (-2,-1))).unsqueeze(-1)
+    ### TODO THIS IS WHERE THE BACKPROP CRASHES ###
+    log_p_E = ((1/(torch.norm(A, p='fro', dim=[-2,-1])-n)) * torch.sum(torch.log(add_e7(torch.sum(E * E_hat_t, -1))), (-2,-1))).unsqueeze(-1)
 
     log_p = - l_A * log_p_A - l_F * log_p_F - l_E * log_p_E
     return log_p