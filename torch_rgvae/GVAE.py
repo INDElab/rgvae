Changed E to softmax, fixed loss.

torch_rgvae/GVAE.py
@@ -76,7 +76,7 @@ class TorchGVAE(nn.Module):
 
         a, e, f = pred[:,:delimit_a], pred[:,delimit_a:delimit_e], pred[:, delimit_e:]
         A = torch.reshape(a, [-1, self.n, self.n])
-        E = torch.reshape(e, [-1, self.n, self.n, self.ea])
+        E = self.softmax(torch.reshape(e, [-1, self.n, self.n, self.ea]))
         F = self.softmax(torch.reshape(f, [-1, self.n, self.na]))
         return A, E, F
 
@@ -95,17 +95,14 @@ class TorchGVAE(nn.Module):
         assert z.shape[-1] == self.z_dim
         a, e, f = self.reconstruct(self.decoder(z))
         a_dist = torch.distributions.Bernoulli(a)
-        a = a_dist.sample()
-        e_dist = torch.distributions.Bernoulli(e)
-        e = e_dist.sample()
+        a_sample = a_dist.sample()
+        e_dist = torch.distributions.Categorical(e)
+        e_dense = e_dist.sample()
         f_dist = torch.distributions.Categorical(f)
         f_dense = f_dist.sample()
-        f_zeros = torch.zeros_like(f)
-        inds = list(zip(np.zeros(f_dense.shape[-1], dtype=int), np.arange(f_dense.shape[-1]), *f_dense.detach().cpu().numpy()))
-        for ind in inds:
-            f_zeros[ind] = 1
 
-        return (a, e, f_zeros)
+
+        return (a_sample, e_dense, f_dense)
 
     def sanity_check(self):
         """
@@ -140,14 +137,7 @@ class TorchGVAE(nn.Module):
         else:
             sanity_2 = 1 - (e_check-e)/e
 
-        # Sanity 3
-        E_check = torch.sum(E.detach().clone(), -1)
-        E_check[E_check > 0] = 1.
-        zero_check = torch.zeros_like(A)
-        zero_check[A == E_check] = 1
-        sanity_3 = (torch.sum(zero_check)/(n**2)).item() 
-
-        return sanity_1 * 100, sanity_2 * 100, sanity_3 * 100
+        return sanity_1 * 100, sanity_2 * 100
 
 
 if __name__ == "__main__":

torch_rgvae/losses.py
@@ -27,7 +27,7 @@ def graph_BCEloss(target, prediction, l_A=1., l_E=1., l_F=1.):
     return loss
 
 
-def mpgm_loss(target, prediction, l_A=1., l_E=1., l_F=1., zero_diag: bool=True):
+def mpgm_loss(target, prediction, l_A=1., l_E=1., l_F=1., zero_diag: bool=False):
     """
     Modification of the loss function described in the GraphVAE paper.
     The difference is, we treat A and E the same as both are sigmoided and F stays as it is softmaxed.
@@ -56,6 +56,7 @@ def mpgm_loss(target, prediction, l_A=1., l_E=1., l_F=1., zero_diag: bool=True):
     # This is the loss part from the paper:
     A_t = torch.transpose(X, 2, 1) @ A @ X     # shape (bs,k,n)
     E_t = torch_batch_dot_v2(torch_batch_dot_v2(X, E, 1, 1, [bs,n,k,d_e]), X, -2, 1, [bs,k,k,d_e])    # target shape is (bs,k,k,d_e)
+    E_hat_t = torch_batch_dot_v2(torch_batch_dot_v2(X, E_hat, -1, 1, [bs,n,k,d_e]), X, -2, 1, [bs,n,n,d_e])
     F_hat_t = torch.matmul(X, F_hat)
 
     term_1 = (1/k) * torch.sum(torch.diagonal(A_t, dim1=-2, dim2=-1) * torch.log(torch.diagonal(A_hat, dim1=-2, dim2=-1)), -1, keepdim=True)
@@ -71,10 +72,13 @@ def mpgm_loss(target, prediction, l_A=1., l_E=1., l_F=1., zero_diag: bool=True):
     term_32 = (1. - A_t) * torch.log(1. - A_hat)
     # Zero diagonal mask:
     mask = torch.ones_like(term_32)
+    # The number of edges we are taking into account.
+    a_edges = k*k
     if zero_diag:
         ind = np.diag_indices(mask.shape[-1])
         mask[:,ind[0], ind[1]] = 0
-    term_3 = (1/(k*(k-1))) * torch.sum((term_31 + term_32) * mask, [1,2]).unsqueeze(-1)
+        a_edges = (k*(k-1))
+    term_3 = (1/a_edges) * torch.sum((term_31 + term_32) * mask, [1,2]).unsqueeze(-1)
     log_p_A = term_1 + term_2 + term_3
 
     # log_p_F  
@@ -85,7 +89,12 @@ def mpgm_loss(target, prediction, l_A=1., l_E=1., l_F=1., zero_diag: bool=True):
     k_zero = k
     if zero_diag:
         k_zero = k - 1
-    log_p_E = ((1/(k*(k_zero))) * torch.sum(torch.sum(E_t * torch.log(E_hat) + (1 - E_t) * torch.log(1 - E_hat), -1) * mask, (-2,-1))).unsqueeze(-1)
+    log_p_E2 = ((1/(k*(k_zero))) * torch.sum(torch.sum(E_t * torch.log(E_hat) + (1 - E_t) * torch.log(1 - E_hat), -1) * mask, (-2,-1))).unsqueeze(-1)
+
+
+    # log_p_E
+    log_p_E = ((1/(torch.norm(A, p=1, dim=[-2,-1]))) * torch.sum(torch.sum(torch.log(no_zero(E * E_hat_t)), -1) * mask, (-2,-1))).unsqueeze(-1)
+
 
     log_p = l_A * log_p_A + l_F * log_p_F + l_E * log_p_E
     return log_p, X