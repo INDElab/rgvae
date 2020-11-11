9e30d6f27dd0e59f04a4d9494d404abc0c6fb3ab
wolf <florian.wolf@student.uva.nl>
2020-11-11 16:08:34 +0100

    More memory for lp.

--- run_lp.py
+++ run_lp.py
@@ -14,14 +14,19 @@ import os
 
 if __name__ == "__main__":
     
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--config', nargs=1,
-    #                     help="JSON file with configurations",
-    #                     type=argparse.FileType('r'))
-    # arguments = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ds', nargs=1,
+                        help="dataset",
+                        type=str)
+    parser.add_argument('--m_path', nargs=1,
+                        help="model path",
+                        type=str)                    
+    args = parser.parse_args()
     # # Loading a JSON object returns a dict.
     # config = json.load(arguments.config)
-    # print(arguments)
+    print(args)
+    dataset = args.ds[0]
+    model_path = args.m_path[0]
 
     # This sets the default torch dtype. Double-power
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -33,39 +38,37 @@ if __name__ == "__main__":
 
     # Parameters. Arg parsing on its way.
     n = 1       # number of triples per matrix ( =  matrix_n/2)
-    batch_size = 2**13        # Choose a low batch size for debugging,.
+    batch_size = 2**15        # Choose a low batch size for debugging,.
     h = 60      # number of hidden dimensions
     seed = 11
     np.random.seed(seed=seed)
     torch.manual_seed(seed)
 
 
-    for dataset in ['fb15k', 'wn18rr']:
-        model_list = list()
-        folder = 'data/{}/models/'.format(dataset)
-        for filename in os.listdir(folder):
-            if filename.endswith(".pt"):
-                model_list.append(filename)
-        # Get data
-        (n2i, i2n), (r2i, i2r), train_set, test_set, all_triples = load_link_prediction_data(dataset, use_test_set=False)
-        d_n = len(n2i)
-        d_e = len(r2i)
-        for model_path in model_list:
-            # Initialize model and optimizer.
-            if 'GCVAE' in model_path.split('_'):
-                model = GCVAE(n*2, d_e, d_n, dataset, z_dim=h).to(device)
-            else:
-                model = GVAE(n*2, d_e, d_n, dataset, z_dim=h).to(device)
+    folder = 'data/{}/models/'.format(dataset)
+    # model_list = list()
+    # for filename in os.listdir(folder):
+    #     if filename.endswith(".pt"):
+    #         model_list.append(filename)
+    # Get data
+    (n2i, i2n), (r2i, i2r), train_set, test_set, all_triples = load_link_prediction_data(dataset, use_test_set=False)
+    d_n = len(n2i)
+    d_e = len(r2i)
 
+    # Initialize model and optimizer.
+    if 'GCVAE' in model_path.split('_'):
+        model = GCVAE(n*2, d_e, d_n, dataset, z_dim=h).to(device)
+    else:
+        model = GVAE(n*2, d_e, d_n, dataset, z_dim=h).to(device)
 
-            model.load_state_dict(torch.load(folder + model_path, map_location=torch.device(device)))
-            print('Saved model loaded.')
-            
-            testsub = torch.tensor(test_set[:2], device=d())
-            truedict = truedicts(all_triples)
+    model.load_state_dict(torch.load(folder + model_path, map_location=torch.device(device)))
+    print('Saved model loaded.')
+    
+    testsub = torch.tensor(test_set, device=d())
+    truedict = truedicts(all_triples)
 
-            lp_results =  link_prediction(model, testsub, truedict, batch_size)
+    lp_results =  link_prediction(model, testsub, truedict, batch_size)
 
-            lp_file_path = 'data/'+dataset+'/lp_results_{}_{}.json'.format(model.name, date.today().strftime("%Y%m%d"))
-            with open(lp_file_path, 'w') as outfile:
-                json.dump(lp_results, outfile)
+    lp_file_path = 'data/'+dataset+'/lp_{}.json'.format(model_path.split('.')[0])
+    with open(lp_file_path, 'w') as outfile:
+        json.dump(lp_results, outfile)
--- xxx.sh
+++ xxx.sh
@@ -2,7 +2,9 @@
 
 git pull
 rm slurm*
-sbatch job_train_fb.sh
-sbatch job_train_wn.sh
-cd ..
+sbatch lp1.sh
+sbatch lp2.sh
+sbatch lp3.sh
+sbatch lp4.sh
+
 # scp -r fwolf@lisa.surfsara.nl:~/results/scratch/rgvae/data/model/GVAE_fb15k_110e_14l_20201110.pt /home/wolf/Desktop/results
