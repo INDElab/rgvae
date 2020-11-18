7ca7f712d00ac8bb159ac095e544b2fd5d510818
wolf <florian.wolf@student.uva.nl>
2020-11-18 15:48:58 +0100

    Job update.

--- lp4.sh
+++ lp4.sh
@@ -3,7 +3,7 @@
 
 #SBATCH -N 1
 #SBATCH -t 33:00:00
-#SBATCH -p gpu_titanrtx
+#SBATCH -p gpu_titanrtx_shared
 #SBATCH --gres=gpu:1
 
 # Loading modules
@@ -18,7 +18,7 @@ export DATA_TO_UNZIP_PATH=$HOME/rgvae.tar.gz
 export DATASET_DIR="datasets"
 export EXPERIMENT_NAME="GVAE_h60_wn"
 export PATH_TO_SOURCE="rgvae"
-export STORE_DIR=$HOME/results1118
+export STORE_DIR=$HOME/results1117
 
 mkdir -p "$STORE_DIR"
 # Copy input data from home to scratch
@@ -28,6 +28,6 @@ cd "$TMPDIR"/"rgvae"
 # pip3 install --user -r requirements.txt
 # pip3 install --user torch==1.7.0+cu101 torchvision==0.8.1+cu101 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip3 install --user -e .
-python3 -u run_lp.py --ds wn18rr --m_path GVAE_wn18rr_110e_12l_20201110.pt --bs_2 15
+python3 -u run_lp.py --ds wn18rr --m_path GVAE_wn18rr_110e_12l_20201110.pt --bs_2 12
 
 tar -czf "$STORE_DIR"/"$EXPERIMENT_NAME".tar.gz "$TMPDIR"/"rgvae"/"data"
