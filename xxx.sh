3b58088e66d365cccf6c0cb170936b4216322126
wolf <florian.wolf@student.uva.nl>
2020-11-20 12:30:57 +0100

    Rm job scripts :heavy_check_mark:

--- xxx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sbatch lp1.sh
-sbatch lp2.sh
-sbatch lp3.sh
-sbatch lp4.sh
-
-# scp -r /home/wolf/Desktop/results/fb15k/GVAE_fb15k_110e_14l_20201110.pt fwolf@lisa.surfsara.nl:~/rgvae/data/model/GVAE_fb15k_110e_14l_20201110.pt 
