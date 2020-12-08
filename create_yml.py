19a3bd25ab58f98442440c0e7dea402ba0480072
wolf <florian.wolf@student.uva.nl>
2020-12-09 00:48:20 +0100

    Variational Distmult implemented :feet:

--- create_yml.py
+++ create_yml.py
@@ -12,32 +12,28 @@ if __name__ == "__main__":
     configs['experiment']['load_model'] = False
     configs['experiment']['load_model_path'] = []
 
-    configs['model_params']['model_name'] = model_name =  'GVAE'    # Choose between [GVAE, GCVAE]
-    configs['model_params']['z_dim'] = 400                          # Latent dimensions
-    configs['model_params']['h_dim'] = 1024                          # Hidden dimensions
+    configs['model_params']['model_name'] = model_name =  'GCVAE'    # Choose between [GVAE, GCVAE]
+    configs['model_params']['z_dim'] = 110                          # Latent dimensions
+    configs['model_params']['h_dim'] = 2048                          # Hidden dimensions
     configs['model_params']['n'] = 1                                # Triples per graph
     configs['model_params']['beta'] = 100                           # betaVAE ratio
-    configs['model_params']['epochs'] = 200
-    configs['model_params']['lr'] = 3e-7
-    configs['model_params']['batch_size_exp2'] = 11                 # 2**(batch_size_exp) fb_max=13 wn_max=12
+    configs['model_params']['epochs'] = 333
+    configs['model_params']['lr'] = 5e-6
+    configs['model_params']['batch_size_exp2'] = 13                 # 2**(batch_size_exp) fb_max=13 wn_max=12
+    configs['model_params']['k'] = 11
 
-    configs['dataset_params']['dataset_name'] = dataset = 'wn18rr'  # Choose between [fb15k, wn18rr]
+    configs['dataset_params']['dataset_name'] = dataset = 'fb15k'  # [fb15k. wn18rr]
 
 
 
-    folder = 'configs/beta/'
+    folder = 'configs/beta/'            # TODO change folder
     if not os.path.isdir(folder):
         os.makedirs(folder)
 
         
-    for beta in [0.1, 1, 10, 100, 1000]:
-        if beta == 0.1:
-            configs['experiment']['exp_name'] = 'tune_{}_{}_b{}'.format(model_name, dataset, '01')  # Most important
-        else:
-            configs['experiment']['exp_name'] = 'tune_{}_{}_b{}'.format(model_name, dataset, beta)  # Most important
-
+    for beta in [1, 100, 1000]:
+        configs['experiment']['exp_name'] = 'tune_{}_{}_b{}'.format(model_name, dataset, beta)  # Most important
         configs['model_params']['beta'] = beta
-        configs['model_params']['lr'] = min([(3e-5 / beta), 1e-7])
         yml_name = '{}.yml'.format(configs['experiment']['exp_name'])
         with open(folder + yml_name,'w') as f:
             yaml.dump(configs, f)
