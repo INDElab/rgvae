184e665b74edf1676be29e187acb2e0ba2845b00
wolf <florian.wolf@student.uva.nl>
2021-01-04 17:20:53 +0100

    Interpolation

--- create_yml.py
+++ create_yml.py
@@ -6,17 +6,17 @@ if __name__ == "__main__":
 
     configs = dict()
 
-    configs['project'] = 'GCVAE_lp'           # TODO change this to the final experiment name for wandb
+    configs['project'] = 'latent_dim_exp'           # TODO change this to the final experiment name for wandb
     configs['train'] = True
-    configs['link_prediction'] = True
+    configs['link_prediction'] = False
     configs['load_model'] = False
     configs['load_model_path'] = []
 
-    configs['model_name'] = model_name =  'GCVAE'    # Choose between ['GVAE', 'GCVAE']
-    configs['z_dim'] = 100                          # Latent dimensions
+    configs['model_name'] = model_name =  'GVAE'    # Choose between ['GVAE', 'GCVAE']
+    configs['z_dim'] = 10                          # Latent dimensions
     configs['h_dim'] = 1024                          # Hidden dimensions 2048, 512, 
     configs['n'] = 1                                # Triples per graph
-    configs['beta'] = 10                           # betaVAE ratio
+    configs['beta'] = 100                           # betaVAE ratio
     configs['adj_argmax'] = True
     configs['perm_inv'] = True
     configs['softmax_E'] = True
@@ -28,18 +28,19 @@ if __name__ == "__main__":
     configs['clip_grad'] = True
     configs['final'] = True
 
-    configs['dataset_name'] = dataset = 'wn18rr'  # ['fb15k', 'wn18rr']
+    configs['dataset_name'] = dataset = 'fb15k'  # ['fb15k', 'wn18rr']
 
 
 
-    folder = 'configs/final_lp/'            # TODO change folder
+    folder = 'configs/LP_sota/'            # TODO change folder
     if not os.path.isdir(folder):
         os.makedirs(folder)
 
         
     # for dataset in ['fb15k', 'wn18rr']:
     #     for l in [10, 100, 1000]:
-    configs['exp_name'] = 'LP{}_{}_p{}'.format(model_name, dataset, '1' if configs['perm_inv'] else '0')  # Most important    '1' if configs['perm_inv'] else '0'
+    # for model_name in ['GVAE', 'GCVAE', 'GCVAE2']:
+    configs['exp_name'] = 'sota{}_{}_p{}'.format(model_name, dataset, '1' if configs['perm_inv'] else '0')  # Most important    '1' if configs['perm_inv'] else '0'
     configs['dataset_name'] = dataset
     # configs['z_dim'] = l
     yml_name = '{}.yml'.format(configs['exp_name'])
